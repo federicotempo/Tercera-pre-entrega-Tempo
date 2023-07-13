c4bd8705574b8b58061a52223b97de53e9ed6ac1
Federico Tempo <federicotempo98@gmail.com>
2023-07-13 11:13:22 -0300

    referencias a las aplicaciones

--- project/apps/cliente/urls.py
+++ project/apps/cliente/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
 
+app_name = "cliente"
+
 urlpatterns = [
     path('', home, name="home"),
 ]
--- project/apps/vehiculo/urls.py
+++ project/apps/vehiculo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
 
+app_name = "vehiculo"
+
 urlpatterns = [
     path('', home, name="home"),
 ]
