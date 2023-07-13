483c204fb808f482d0cafd01d1e189ab63b1a117
Federico Tempo <federicotempo98@gmail.com>
2023-07-13 00:43:47 -0300

    creacion del template de cliente y preparcion de urls, index y views para cliente

--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Cliente
 # Create your views here.
+
+def home(request):
+    clientes_registros = Cliente.objects.all()
+    contexto = {"clientes": clientes_registros}
+    return render(request, "cliente/index.html", contexto)
--- project/config/urls.py
+++ project/config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #! include sirve para traer urls de las aplicaciones
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("cliente.urls")), 
 ]
