48082e9634f99be3b5b6cc1a0f7b1da44ade14c6
Federico Tempo <federicotempo98@gmail.com>
2023-07-13 14:21:08 -0300

    cambios en template de vehiculo y models de cliente

--- project/apps/cliente/models.py
+++ project/apps/cliente/models.py
@@ -12,7 +12,7 @@ class Cliente(models.Model):
     apellido = models.CharField(max_length=100)
     nacimiento = models.DateField(null=True)
     documento = models.IntegerField()
-    vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.SET_NULL, null=True)
+    vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.SET_NULL, null=True, blank = True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
