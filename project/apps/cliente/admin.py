b51e5ec260158b453e7a52ddc739342f33967c63
Federico Tempo <federicotempo98@gmail.com>
2023-07-12 23:48:21 -0300

    modificaciones en models de cliente

--- project/apps/cliente/admin.py
+++ project/apps/cliente/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Cliente, Vehiculo
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Vehiculo)
--- project/apps/cliente/models.py
+++ project/apps/cliente/models.py
@@ -7,19 +7,12 @@ class Vehiculo(models.Model):
     def __str__(self):
         return self.nombre
 
-class Dias(models.Model):
-    dias = models.IntegerField
-    
-    def __str__(self):
-        return self.dias
-
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     nacimiento = models.DateField(null=True)
     documento = models.IntegerField()
     vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.SET_NULL, null=True)
-    dias_id = models.ForeignKey(Dias, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
