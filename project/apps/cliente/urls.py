108beece4ff8e7639baee9280f6a2d3bd2671da6
Federico Tempo <federicotempo98@gmail.com>
2023-07-13 13:50:50 -0300

    formulario agregado

--- project/apps/cliente/urls.py
+++ project/apps/cliente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, crear_cliente
 
 app_name = "cliente"
 
 urlpatterns = [
     path('', home, name="home"),
+    path('crear_cliente', crear_cliente, name = "crear_cliente"),
 ]
--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+
 from .models import Cliente
+from .forms import ClienteForm
+
 # Create your views here.
 
 def home(request):
     clientes_registros = Cliente.objects.all()
     contexto = {"clientes": clientes_registros}
     return render(request, "cliente/index.html", contexto)
+
+def crear_cliente(request: HttpRequest) -> HttpResponse:
+    
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cliente:home")
+    else:
+        form = ClienteForm()
+    return render(request, "cliente/crear.html", {"form": form})
