6e52b05a30121f6e76cbd8437501aeeab1824b21
Federico Tempo <federicotempo98@gmail.com>
2023-07-13 21:16:39 -0300

    modificando la app clientes

--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -6,18 +6,20 @@ from .forms import ClienteForm
 
 # Create your views here.
 
+
 def home(request):
     clientes_registros = Cliente.objects.all()
     contexto = {"clientes": clientes_registros}
     return render(request, "cliente/index.html", contexto)
 
+
 def crear_cliente(request: HttpRequest) -> HttpResponse:
-    
+
     if request.method == "POST":
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("cliente:home")
+            return redirect("Home:home")
     else:
         form = ClienteForm()
     return render(request, "cliente/crear.html", {"form": form})
